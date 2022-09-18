b5df1a3467140a13ad977df2c4bdbfc83967b00f
k0pernicus <dev@0xc0ff33.me>
2022-09-18 11:14:10 +0200

    Initial Commit

---
+++ FrameTech/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FrameTech
+//
+//  Created by Antonin on 18/09/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
