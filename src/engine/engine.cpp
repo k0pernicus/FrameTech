b28a9fa43a5aa8475c23a684b415663738ea7794
k0pernicus <dev@0xc0ff33.me>
2022-10-03 23:04:25 +0200

    Fixed a big issue destroying a unique ptr

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -75,7 +75,7 @@ FrameTech::Engine::~Engine()
 {
     Log("< Closing the Engine object...");
     m_swapchain = nullptr;
-    m_render->~Render();
+    m_render = nullptr;
     m_graphics_device.Destroy();
     if (m_graphics_instance)
         vkDestroyInstance(m_graphics_instance, nullptr);
