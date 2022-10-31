fecbde82a5c610403a2a51cfbf02596591a2d50b
k0pernicus <dev@0xc0ff33.me>
2022-10-31 21:21:13 +0100

    Added new overall FPS limits as constant values

--- src/project.hpp
+++ src/project.hpp
@@ -13,13 +13,22 @@
 #include <cstdlib>
 #include <optional>
 
+/// @brief Limits of 120 FPS
+constexpr uint8_t FPS_LIMIT_120 = 120;
+/// @brief Limits of 60 FPS
+constexpr uint8_t FPS_LIMIT_60 = 60;
+/// @brief Limits of 30 FPS
+constexpr uint8_t FPS_LIMIT_30 = 30;
+/// @brief No FPS limit
+constexpr std::optional<uint8_t> NO_FPS_LIMIT = std::nullopt;
+
 namespace Project
 {
 
     /// @brief The name of the application
     constexpr const char* APPLICATION_NAME = "Example";
-    /// @brief Pause / limit the Application to 60 FPS
-    const std::optional<uint8_t> APPLICATION_FPS_LIMIT = 60;
+    /// @brief Pause / limit the Application to XXX FPS
+    const std::optional<uint8_t> APPLICATION_FPS_LIMIT = FPS_LIMIT_120;
     /// @brief Major version number of the application
     constexpr uint8_t const APPLICATION_VERSION_MAJOR_NUMBER = 0;
     /// @brief Minor version number of the application
