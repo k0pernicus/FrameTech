812a0dd0e74864af71d2d90d4c165a0fb01e0a00
Antonin Carette <dev@carette.xyz>
2023-04-28 15:31:11 +0200

    UBO definition in a dedicated cpp header file

---
+++ src/engine/graphics/definitions.hpp
@@ -0,0 +1,24 @@
+//
+//  definitions.hpp
+//  FrameTech
+//
+//  Created by Antonin on 28/04/2023.
+//
+
+#pragma once
+#ifndef definitions_h
+#define definitions_h
+
+#include <glm/glm.hpp>
+
+// Compatibility layer with shaders
+// No need any namespace here
+
+typedef struct UniformBufferObject
+{
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 proj;
+} UniformBufferObject;
+
+#endif // command_h
