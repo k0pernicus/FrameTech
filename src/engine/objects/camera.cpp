22873c17337ad44c7865986959395c0efa96acf4
k0pernicus <dev@carette.xyz>
2023-05-07 00:37:38 +0200

    Basic camera implementation
    Only handles the direction for the moment

--- src/engine/objects/camera.cpp
+++ src/engine/objects/camera.cpp
@@ -6,3 +6,15 @@
 //
 
 #include "camera.hpp"
+
+glm::vec3 frametech::engine::Camera::getDirection() const noexcept {
+	return m_direction;
+}
+
+void frametech::engine::Camera::setDirection(const glm::vec3& new_direction) noexcept {
+	m_direction = new_direction;
+}
+
+void frametech::engine::Camera::resetDirection() noexcept {
+	m_direction = m_original_direction;
+}
--- src/engine/objects/camera.hpp
+++ src/engine/objects/camera.hpp
@@ -9,4 +9,22 @@
 #ifndef _camera_hpp
 #define _camera_hpp
 
+#include <glm/glm.hpp>
+
+namespace frametech {
+	namespace engine {
+		class Camera {
+		public:
+			Camera(glm::vec3 direction = glm::vec3(0.0f)) : m_original_direction(direction), m_direction(direction) {};
+			glm::vec3 getDirection() const noexcept;
+			void resetDirection() noexcept;
+			void setDirection(const glm::vec3& new_direction) noexcept;
+		private:
+			glm::vec3 m_original_direction;
+			glm::vec3 m_direction;
+			// TODO : Camera position
+		};
+	}
+}
+
 #endif // _camera_hpp
