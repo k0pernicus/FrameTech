2ed628e42f1f64d0ea49ff26fbc725dbc738be1a
Antonin Carette <dev@carette.xyz>
2023-05-21 15:29:23 +0200

    Implemented a basic World object (for tests purposes)

--- src/engine/objects/world.cpp
+++ src/engine/objects/world.cpp
@@ -6,3 +6,30 @@
 //
 
 #include "world.hpp"
+#include "../ftstd/debug_tools.h"
+
+frametech::engine::MovableInterface* frametech::engine::World::getSelectedObject() noexcept
+{
+    return m_selected_object;
+}
+
+void frametech::engine::World::setSelectedObject(frametech::engine::MovableInterface* object) noexcept
+{
+    if (nullptr == object)
+    {
+        LogW("tried to set a NULL object as selected editor object, forbidden");
+        return;
+    }
+    m_selected_object = object;
+}
+
+frametech::engine::Camera& frametech::engine::World::getMainCamera() noexcept
+{
+    return m_main_camera;
+}
+
+void frametech::engine::World::init() noexcept
+{
+    // The default selected object of the world is the main camera
+    m_selected_object = static_cast<frametech::engine::MovableInterface*>(&m_main_camera);
+}
--- src/engine/objects/world.hpp
+++ src/engine/objects/world.hpp
@@ -9,4 +9,28 @@
 #ifndef _world_hpp
 #define _world_hpp
 
+#include "camera.hpp"
+#include "movable.hpp"
+#include <optional>
+
+namespace frametech
+{
+    namespace engine
+    {
+        class World
+        {
+        public:
+            void init() noexcept;
+            frametech::engine::MovableInterface* getSelectedObject() noexcept;
+            void setSelectedObject(frametech::engine::MovableInterface* object) noexcept;
+            frametech::engine::Camera& getMainCamera() noexcept;
+
+        private:
+            // TODO: should not consider only one camera here, move it to a smart system
+            frametech::engine::Camera m_main_camera{};
+            frametech::engine::MovableInterface* m_selected_object = nullptr;
+        };
+    } // namespace engine
+} // namespace frametech
+
 #endif // _world_hpp
